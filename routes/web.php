2ab58a8ff9dfdf7c73836e3cf266c98d9599d1e9
Said Hammane <said.hammane1@gmail.com>
2026-02-08 13:50:02 +0100

    Routing to french

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/en');
+Route::redirect('/', '/fr');
 
 Route::prefix('{locale}')
     ->where(['locale' => 'en|fr'])
